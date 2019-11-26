256622f1ad1641d97375f7a7d3a6cc894a5fc55a
yuzubot <yuzu@yuzu-emu.org>
2019-11-26 03:21:42 +0000

    "Merge Tagged PR 2365"

--- src/core/hle/kernel/svc.cpp
+++ src/core/hle/kernel/svc.cpp
@@ -584,7 +584,8 @@ static ResultCode ArbitrateUnlock(Core::System& system, VAddr mutex_addr) {
     }
 
     auto* const current_process = system.Kernel().CurrentProcess();
-    return current_process->GetMutex().Release(mutex_addr);
+    return current_process->GetMutex().Release(mutex_addr,
+                                               system.CurrentScheduler().GetCurrentThread());
 }
 
 enum class BreakType : u32 {
@@ -1621,12 +1622,13 @@ static ResultCode WaitProcessWideKeyAtomic(Core::System& system, VAddr mutex_add
     std::shared_ptr<Thread> thread = handle_table.Get<Thread>(thread_handle);
     ASSERT(thread);
 
-    const auto release_result = current_process->GetMutex().Release(mutex_addr);
+    Thread* current_thread = system.CurrentScheduler().GetCurrentThread();
+
+    const auto release_result = current_process->GetMutex().Release(mutex_addr, current_thread);
     if (release_result.IsError()) {
         return release_result;
     }
 
-    Thread* current_thread = system.CurrentScheduler().GetCurrentThread();
     current_thread->SetCondVarWaitAddress(condition_variable_addr);
     current_thread->SetMutexWaitAddress(mutex_addr);
     current_thread->SetWaitHandle(thread_handle);
