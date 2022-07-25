169edeb089f32dd197df9eaa0fffbcab31d082b5
yuzubot <yuzu@yuzu-emu.org>
2022-07-25 12:02:18 +0000

    "Merge Tagged PR 8592"

--- src/yuzu/main.h
+++ src/yuzu/main.h
@@ -161,6 +161,8 @@ signals:
     void WebBrowserExtractOfflineRomFS();
     void WebBrowserClosed(Service::AM::Applets::WebExitReason exit_reason, std::string last_url);
 
+    void SigInterrupt();
+
 public slots:
     void OnLoadComplete();
     void OnExecuteProgram(std::size_t program_index);
@@ -246,6 +248,12 @@ private:
     void RequestGameExit();
     void RequestGameResume();
     void closeEvent(QCloseEvent* event) override;
+
+#ifdef __linux__
+    void SetupSigInterrupts();
+    static void HandleSigInterrupt(int);
+    void OnSigInterruptNotifierActivated();
+#endif
 
 private slots:
     void OnStartGame();
@@ -415,6 +423,9 @@ private:
     bool is_tas_recording_dialog_active{};
 
 #ifdef __linux__
+    QSocketNotifier* sig_interrupt_notifier;
+    static std::array<int, 3> sig_interrupt_fds;
+
     QDBusObjectPath wake_lock{};
 #endif
 
